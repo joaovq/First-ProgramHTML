f504340e7726f2d0de37414240ff83557e0b5f81
João Vítor <vitorsnta90@gmail.com>
2022-06-24 19:59:24 -0300

    first commit

--- .vscode/extensions/pivotal.vscode-spring-boot-1.36.0/out/lib/Main.d.ts
+++
@@ -1,4 +0,0 @@
-import * as VSCode from 'vscode';
-import { ExtensionAPI } from "./api";
-/** Called when extension is activated */
-export declare function activate(context: VSCode.ExtensionContext): Thenable<ExtensionAPI>;
--- .vscode/extensions/pivotal.vscode-spring-boot-1.36.0/out/lib/apiManager.d.ts
+++
@@ -1,9 +0,0 @@
-import { LanguageClient } from "vscode-languageclient/node";
-import { ExtensionAPI } from "./api";
-export declare class ApiManager {
-    api: ExtensionAPI;
-    private onDidLiveProcessConnectEmitter;
-    private onDidLiveProcessDisconnectEmitter;
-    private onDidLiveProcessUpdateEmitter;
-    constructor(client: LanguageClient);
-}
